d6a4f6556c87d5ee71401e68b82869009fe22a98
heytitle <pat.chormai@gmail.com>
2016-01-24 14:58:46 +0100

    slider2

--- app/scripts/line-slider.js
+++ app/scripts/line-slider.js
@@ -1,56 +1,11 @@
 var chartID        = "#chart";
-var X_AXIS_PADDING = 30;
-var margin = {top: 20, right: 20, bottom: 30, left: 50};
-var width = 960 - margin.left - margin.right;
-var height = 200 - margin.top - margin.bottom + X_AXIS_PADDING;
 
-var MIN_YEAR = 2007;
-var MAX_YEAR = 2015;
+// var MIN_YEAR = 2007;
+// var MAX_YEAR = 2015;
 // var ticks = (MAX_YEAR - MIN_YEAR)*4 - 1;
-var colors =["steelblue","orange","#31a354"];
-var FOCUS_YEAR = 2010;
 
-var x = d3.scale.linear()
-    .range([0, width]);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .tickPadding(X_AXIS_PADDING)
-    .tickFormat(d3.format("d"))
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .ticks(5)
-    .orient("left");
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) {
-        return y(d.close); }
-    );
-
-var svg = d3.select(chartID).append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom + X_AXIS_PADDING )
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("data.tsv", type, function(error, data) {
-    console.log(data);
-  if (error) throw error;
-  createGraph(data);
-});
-
-var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 50])
-  .html(function(d) {
-    return "<strong>Value </strong> <span style='color:red'>" + d.close + "</span>";
-})
 
 function createGraph( data ) {
   x.domain(d3.extent(data, function(d) { return d.year; }));
@@ -58,127 +13,19 @@ function createGraph( data ) {
 
   var timeSelector = svg.append("g");
   var yTimeSelector = height + 20;
-  timeSelector.append("line")
-      .attr("x1", 0 )
-      .attr("x2", width )
-      .attr("y1", yTimeSelector )
-      .attr("y2", yTimeSelector )
-      .attr("class", "year-selector" )
-      .style("stroke", "black");
-
-  timeSelector.append("g")
-    .selectAll('circle')
-    .data(data)
-    .enter().append('circle')
-    .attr("class", "year-bullet")
-    .classed("selected", function(d){
-        return d.year == FOCUS_YEAR;
-    })
-    .attr("cx", function(d) {
-        return x(d.year);
-      })
-    .attr("cy", yTimeSelector )
-    .attr("r", 8 )
-    .on("click", function(d,i){
-        var xPos = x(d.year);
-        var yPos = y(d.close);
-        currentYearLine.attr("x1", xPos )
-            .attr("x2", xPos )
-            .attr("y1", yPos );
 
 
-        d3.select(chartID)
-            .selectAll(".year-bullet")
-            .classed("selected",function(e,j){
-                return i == j;
-            });
-    });
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + -20 + ",0)")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end");
-
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("stroke", colors[1])
-      .attr("d", line);
 
     svg.call(tip);
 
-   var focusLine = svg.append("line")
-        .attr("class", "focus-line")
-        .attr("x1", 5)
-        .attr("y1", 0)
-        .attr("x2", 50)
-        .attr("y2", height + X_AXIS_PADDING/4 )
-        .style("opacity", 0 )
 
     var currentX = x(FOCUS_YEAR);
     var currentValue = _.find( data, function(d){
             return d.year == FOCUS_YEAR
         });
-    var currentYearLine = svg.append("line")
-        .attr("class", "focus-line current" )
-        .attr("x1", currentX )
-        .attr("x2", currentX )
-        .attr("y1", y(currentValue.close) )
-        .attr("y2", height + X_AXIS_PADDING/4 )
 
 
   var radius = 5;
-  svg.append("g")
-    .attr("class", "line-point")
-    .selectAll('circle')
-    .data(data)
-    .enter().append('circle')
-    .attr("cx", function(d) {
-        return x(d.year);
-      })
-    .attr("cy", function(d, i) { return y(d.close) })
-    .attr("r", radius )
-    .style("fill", colors[1] )
-    .on("mouseover", function(d,i) {
-        tip.show(d);
 
-        var positionX = x(d.year);
-        var positionY = y(d.close);
-        focusLine
-            .attr("x1", positionX )
-            .attr("x2", positionX )
-            .attr("y1", positionY )
-            .style("opacity", 0.7 );
-    })
-    .on("mouseout", function(d) {
-        tip.hide(d);
-        focusLine.style("opacity", 0 );
-    });
-
-    d3.select("#timeline-panel")
-        .select("ul")
-        // .enter()
-        .append("li")
-        .html(function(d){
-            return "<li><span>●</span>Thailand</li>";
-        })
-        .select("span")
-        .style("color","red");
-}
-
-function type(d) {
-  d.year =  +d.year;
-  d.close = +d.close;
-  return d;
 }
--- app/scripts/slider.js
+++ app/scripts/slider.js
@@ -1,86 +1,313 @@
-var MIN_YEAR = 2007;
-var MAX_YEAR = 2015;
-var ticks = (( MAX_YEAR - MIN_YEAR  )* 4 ) ;
-var slider = d3
-    .slider()
-    .axis(
-        d3
-        .svg
-        .axis()
-        .ticks(ticks)
-        .tickFormat(function(d){
-            var year = d%4;
-            if( year == 0 ) {
-                return MIN_YEAR + Math.floor(d/4);
-            }else {
-                return '';
-            }
+var ColorProvider = {
+    maps: {},
+    availableColors : ["#31a354", "orange" ,"steelblue"],
+    colorForKey: function(k){
+        if( !this.maps[k] ) {
+            var color = this.availableColors.pop();
+            this.maps[k] = color;
+        }
+        return this.maps[k];
+    },
+    releaseColor: function(k){
+        var color = this.maps[k];
+        if( color ){
+            this.availableColors.push(color);
+            delete this.maps[k];
+        }
+    }
+}
+
+function TimelineGraph( id, options ){
+    var self     = this;
+    this.element = d3.select(id);
+    this.datasets = [];
+
+    var FOCUS_YEAR = 2010;
+
+    var MIN_YEAR       = 2007;
+    var MAX_YEAR       = 2015;
+    var YEAR_RANGE     = [MIN_YEAR, MAX_YEAR -1 ];
+    var X_AXIS_PADDING = 30;
+
+    var boxWidth = this.element.node().getBoundingClientRect().width;
+    var margin   = {top: 20, right: 20, bottom: 30, left: 50};
+    var width    = boxWidth - margin.left - margin.right;
+    var height   = 200 - margin.top - margin.bottom + X_AXIS_PADDING;
+
+    this.height  = height;
+
+    this.xScale = d3.scale.linear()
+        .domain(YEAR_RANGE)
+        .range([0, width]);
+
+    var xScale = this.xScale;
+
+
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .tickPadding(X_AXIS_PADDING)
+        .tickFormat(d3.format("d"))
+        .orient("bottom");
+
+    this.svg = this.element.select("#chart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom + X_AXIS_PADDING )
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    this.svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    var timeSelector = this.svg.append("g");
+    var yTimeSelector = height + 20;
+    timeSelector.append("line")
+      .attr("x1", 0 )
+      .attr("x2", width )
+      .attr("y1", yTimeSelector )
+      .attr("y2", yTimeSelector )
+      .attr("class", "year-selector" )
+      .style("stroke", "black");
+
+    var currentX = xScale(FOCUS_YEAR);
+    var currentYearLine = this.svg.append("line")
+        .attr("class", "focus-line current" )
+        .attr("x1", currentX )
+        .attr("x2", currentX )
+        .attr("y1", 0 )
+        .attr("y2", height + X_AXIS_PADDING/4 )
+
+    timeSelector.append("g")
+        .selectAll('circle')
+        .data(d3.range(MIN_YEAR, MAX_YEAR))
+        .enter().append('circle')
+        .attr("class", "year-bullet")
+        .classed("selected", function(d){
+            return d== FOCUS_YEAR;
         })
-    )
-    .min(0)
-    .max(ticks-1)
-    .step(1)
-    .on('slide', function(evt,val){
-        var year = MIN_YEAR + Math.floor(val/4);
-        var quarter = ( val % 4 ) + 1;
+        .attr("cx", function(d) {
+            return xScale(d);
+          })
+        .attr("cy", yTimeSelector )
+        .attr("r", 8 )
+        .on("click", function(d,i){
+            var xPos = xScale(d);
+            // var yPos = self.highestValueForYear(d);
+            currentYearLine.attr("x1", xPos )
+                .attr("x2", xPos )
+                .attr("y1", 0 );
+
+
+            d3.select("#chart")
+                .selectAll(".year-bullet")
+                .classed("selected",function(e,j){
+                    return i == j;
+                });
+        });
+
+    this.tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 50])
+        .html(function(data) {
+            return JSON.stringify(data);
+        });
+
+    this.svg.call(this.tip);
+
+    this.focusLine = this.svg.append("line")
+        .attr("class", "focus-line")
+        .attr("x1", 5)
+        .attr("y1", 0)
+        .attr("x2", 50)
+        .attr("y2", height + X_AXIS_PADDING/4 )
+        .style("opacity", 0 )
+
+
+    this.highestValueForYear = function(year){
+        var data = _.filter( _.flatten(self.datasets), function(d){
+            console.log(d);
+            return d.year == year;
+        });
+
+        return _.max( data, function() { return d.number } );
+    }
+
+    this.graphs = _.map( ['absolute-chart'], function(id){
+        return new LineGraph( self, id );
     });
-d3.select('#year-slider').call(slider);
+
+}
+
+TimelineGraph.prototype.addData = function(data){
+    var self    = this;
+    var country = data[0].country;
+    if( _.find( self.datasets, function(d){ return d[0].country == country } ) ){
+        console.log("country in the list already");
+        return;
+    }
+
+    /* Legend */
+    var element = this.element;
+    var index   = this.datasets.length;
+
+    var legend = element.select("ul")
+        .append("li")
+        .attr("class", "legend" )
+        .html(function(d){
+            return "<li><span>●</span>" + country + " <span class=\"close\">[x]</span></li>";
+        })
+
+    legend.select("span.close")
+        .on("click",function(){
+            console.log(index);
+            self.removeData(country);
+            legend.remove();
+            console.log('xxx');
+        })
+
+    legend.select("span")
+        .style("color", ColorProvider.colorForKey(country) );
+
+    /* Render Both Graph */
+    this.datasets.push(data);
+    this.render();
+}
+
+TimelineGraph.prototype.removeData = function(country){
+    var index = _.findIndex( this.datasets, function(d){
+        return country == d[0].country;
+    });
+    this.datasets.splice(index, 1 );
+
+    this.render();
+}
+
+TimelineGraph.prototype.render = function(){
+    var self = this;
+    _.each( this.graphs, function(g){
+        g.render(self.datasets);
+    });
+}
+
+function LineGraph( parent, id ){
+    this.parent = parent;
+    this.element = parent.svg.append("g").attr("id", id );
+
+    var yScale = d3.scale.linear()
+        .range([ parent.height, 0]);
+
+}
+
+LineGraph.prototype.render = function( datasets ) {
+    this.element.selectAll("*").remove();
+
+    var tip    = this.parent.tip;
+    var xScale = this.parent.xScale;
+    var yScale = d3.scale.linear()
+        .range([ this.parent.height, 0]);
+    var focusLine= this.parent.focusLine;
+
+    var flattenedDataset = _.flatten(datasets);
+    var highestValue = _.maxBy( flattenedDataset, 'number' );
+    console.log(highestValue);
+
+    yScale.domain([0, highestValue.number ]);
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .ticks(5)
+        .tickFormat( d3.format('.2s') )
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function(d) { return xScale(d.year); })
+        .y(function(d) {
+            return yScale(d.number); }
+        );
+
+    this.element.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + -20 + ",0)")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em");
+
+    this.element.selectAll(".country")
+        .data(datasets)
+        .enter()
+        .append("path")
+        .attr("class", "line")
+        .attr("stroke", function(d){
+            return ColorProvider.colorForKey(d[0].country);
+        })
+        .attr("d", line);
+
+    this.element.append("g")
+        .attr("class", "line-point")
+        .selectAll('circle')
+        .data(flattenedDataset)
+        .enter()
+        .append('circle')
+        .attr("cx", function(d) {
+            return xScale(d.year);
+        })
+        .attr("cy", function(d, i) { return yScale(d.number) })
+        .attr("r", 5 )
+        .style("fill", function(d){
+            return ColorProvider.colorForKey(d.country);
+        }).on("mouseover", function(d,i) {
+            var data = _.filter( flattenedDataset, function( i ){
+                return i.year == d.year;
+            });
+
+            tip.show(data);
+
+            var max = _.max( data, function(d){ return d.number } );
+
+            var positionX = xScale(d.year);
+            var positionY = yScale(max.number);
+            focusLine
+                .attr("x1", positionX )
+                .attr("x2", positionX )
+                .attr("y1", positionY )
+                .style("opacity", 0.7 );
+        })
+        .on("mouseout", function(d) {
+            tip.hide(d);
+            focusLine.style("opacity", 0 );
+        });
+}
+
+function type(d) {
+  d.year =  +d.year;
+  d.number = +d.number;
+  return d;
+}
 
 
-nv.addGraph(function() {
-  var chart = nv.models.lineChart()
-                // .margin({left: 30})  //Adjust chart margins to give the x-axis some breathing room.
-                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-                // .transitionDuration(350)  //how fast do you want the lines to transition?
-                // .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                // .showYAxis(true)        //Show the y-axis
-                // .showXAxis(true)        //Show the x-axis
-  ;
+var datasets = [[],[]];
+var timeline = new TimelineGraph("#timeline-panel");
 
-  // chart.xAxis     //Chart x-axis settings
-      // .axisLabel('Time (ms)')
-      // .tickFormat(function(){return""});
+d3.tsv("data.tsv", type, function(error, data) {
+  if (error) throw error;
 
-  chart.yAxis     //Chart y-axis settings
-  //     .axisLabel('Voltage (v)')
-      .tickFormat(d3.format('.02f'));
+  for( var i = 0; i<data.length; i++ ){
+      var index = 0;
+      if( data[i].country == "India" ){
+          index = 1;
+      }
+      datasets[index].push(data[i]);
 
-  /* Done setting the chart up? Time to render it!*/
-  var myData = sinAndCos();   //You need data...
-
-  d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.
-      .datum(myData)         //Populate the <svg> element with chart data...
-      .call(chart);          //Finally, render the chart!
-
-  //Update the chart when window resizes.
-  nv.utils.windowResize(function() { chart.update() });
-  return chart;
-});
-
-function sinAndCos() {
-  var sin = [],sin2 = [],
-      cos = [];
-
-  //Data is represented as an array of {x,y} pairs.
-  for (var i = 0; i < ticks; i++) {
-    sin.push({x: i, y: Math.abs(Math.sin(i/10))});
-    sin2.push({x: i, y: Math.abs(Math.sin(i/10) *0.25 + 0.5)});
-    // cos.push({x: i, y: .5 * Math.cos(i/10)});
   }
 
-  //Line chart data should be sent as an array of series objects.
-  return [
-    {
-      values: sin,      //values - represents the array of {x,y} data points
-      key: 'Thailand', //key  - the name of the series.
-      color: '#ff7f0e'  //color - optional: choose your own line color.
-    },
-    {
-      values: sin2,
-      key: 'India',
-      color: '#7777ff',
-      // area: true      //area - set to true if you want this line to turn into a filled area chart.
-    }
-  ];
-}
+  timeline.addData( datasets[0] );
+
+  // data
+  // self.datasets = dataset;
+  // l.render( dataset );
+});
 
