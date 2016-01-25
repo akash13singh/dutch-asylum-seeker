b75bc3f97765ab446b98b45c3e41ec56d43d4521
akash13singh <aakashsingh@gmail.com>
2016-01-25 18:39:58 +0100

    Map Improved

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,21 +1,29 @@
+
 function Map( id ){
     this.element = d3.select(id);
 
-    var rect = this.element.node().getBoundingClientRect();
-    var width  = rect.width;
-    var height = width/2;
-
+    /*var rect = document.getElementById('map').offsetWidth
+    this.width  = rect.width;
+    this.height = rect.height;*/
+    
+    this.width  = document.getElementById('map').offsetWidth;
+    this.height = this.width/2;
+    
     var projection = d3.geo.mercator()
-        .translate([(width/2), (height/2)])
-        .scale( width / 2 / Math.PI);
+    	.translate([(this.width/2)-30, (this.height/2)+50])
+    	.scale( this.width / 2 / Math.PI);
+
+    /*var projection = d3.geo.mercator()
+    	.center([80,40])
+        .translate([(this.width/2), (this.height/2)])
+    	.scale( this.width / 3 / Math.PI) */
 
     var path = d3.geo.path().projection(projection);
     this.path = path;
 
     var svg = this.element.append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .on("click", function(){ alert("xx") })
+      .attr("width", this.width)
+      .attr("height", this.height)
       .append("g");
 
     this.svg = svg;
@@ -23,35 +31,41 @@ function Map( id ){
     var tooltip = this.element
         .append("div")
         .attr("class", "tooltip hidden");
+        
     this.tooltip = tooltip;
 
     this.topo = null;
 }
 
-Map.prototype.draw = function(data){
+Map.prototype.draw = function(){
     var self     = this;
-    var color    = ["#D4B9DA","#C994C7","#DF65B0","#DD1C77","#980043"];
-    var quantize = d3.scale.quantize()
-        .domain([0, 9000])
-        .range(d3.range(5).map(function(i) { return color[i] }));
-
-    var country = this.svg.append("g")
+    
+    var country = this.svg
         .selectAll(".country").data(this.topo);
 
     var path = this.path;
+
+	var graticule = d3.geo.graticule();
+	this.svg.append("path")
+     .datum(graticule)
+     .attr("class", "graticule")
+     .attr("d", path);
+
+
+    this.svg.append("path")
+   .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
+   .attr("class", "equator")
+   .attr("d", path);
 
     country.enter().insert("path")
         .attr("class", "country")
         .attr("d", path )
         .attr("id", function(d,i) { return d.id; })
-        .attr("title", function(d,i) { return d.properties.name; })
-        .style("fill", function(d, i) {
-            if(data[d.properties.name]){
-                return quantize(data[d.properties.name]['2007']['Total']);
-            }
-            return "#DDE7EB";
-        });
+        .attr("title", function(d,i) { return d.properties.name; });
 
+    //fill color
+    this.colorMap(2013);
+    
     //offsets for tooltips
     var boundary = this.element.node().getBoundingClientRect();
     var offsetL = boundary.left+20;
@@ -69,16 +83,103 @@ Map.prototype.draw = function(data){
       .on("mouseout",  function(d,i) {
         self.tooltip.classed("hidden", true);
       });
+	
+	//click
+	country.on("click",function(d,i){
+		window.alert(d.properties.name);
+	});
+	
+	
+      
 }
 
+Map.prototype.colorMap = function(year){
+    var self     = this;
+    var present = [];
+    //Define default colorbrewer scheme
+	var colorSchemeSelect = "Greens";
+	var colorScheme = colorbrewer[colorSchemeSelect]; 
+
+	//define default number of quantiles
+	var quantiles = 5;
+
+	//Define quantile scale to sort data values into buckets of color
+	var color = d3.scale.quantile()
+	   .range(colorScheme[quantiles]);
+	   
+    var colorDomain =  [];
+    var minDomain = 999999;
+    var maxDomain = 0;
+    for (var key in asylum){
+    	present.push(key);  
+    	colorDomain.push[asylum[key][year]["Total"]];
+    	if(minDomain > asylum[key][year]["Total"]){
+    		minDomain = asylum[key][year]["Total"];
+    	}
+    	if(maxDomain < asylum[key][year]["Total"]){
+    		maxDomain = asylum[key][year]["Total"];
+    	}
+    }
+    color.domain([minDomain,maxDomain]);
+    
+    console.log("minmax"+"::"+minDomain+"::"+maxDomain);
+    
+    this.svg
+        .selectAll(".country")
+        .style("fill", function(d, i) {
+            if(asylum[d.properties.name]){         
+                return color(asylum[d.properties.name][year]['Total']);
+            }
+            return "#DDE7EB";
+        });
+    
+    var legend = this.svg.selectAll('g.legend')
+		.data(color.range())
+		.enter()
+		.append('g').attr('class', 'legend');
+
+	legend
+		.append('rect')
+		.attr("x",this.width - (this.width-30))
+		.attr("y", function(d, i) {		  
+		   return ((self.height-200)+(i * 20));
+		})
+	   .attr("width", 10)
+	   .attr("height", 10)
+	   .style("stroke", "black")
+	   .style("stroke-width", 1)
+	   .style("fill", function(d){return d;}); 
+		   //the data objects are the fill colors
+
+	legend
+		.append('text')
+		.attr("x", this.width - (this.width-45)) //leave 5 pixel space after the <rect>
+		.attr("y", function(d, i) {
+		   return ((self.height-200)+(i * 20));
+		})
+		.attr("dy", "0.8em") //place text one line *below* the x,y point
+		.text(function(d,i) {
+		    var extent = color.invertExtent(d);
+		    //extent will be a two-element array, format it however you want:
+		    var format = d3.format("0.2f");
+		    return format(+extent[0]) + " - " + format(+extent[1]);
+		});
+
+	//console.log(present);
+	//downloading json
+    /*var url = 'data:text/json;charset=utf8,' + encodeURIComponent(present);
+	window.open(url, '_blank');
+	window.focus();*/	
+	
+}
 
 queue().defer(d3.json, "data/world-topo-min.json")
 	.defer(d3.csv, "data/dutch.csv")
     .await(ready);
 
 var map = new Map("#map");
-
 var asylum = {};
+
 function ready(error,world, asylumRequests){
     var countries = topojson.feature(world, world.objects.countries).features;
     map.topo = countries;
@@ -89,7 +190,7 @@ function ready(error,world, asylumRequests){
         //console.log(obj);
         if(asylum[obj["Country"]]== null){
         	 tmp ={};
-        	 //TODO : remove later done to simplify json building
+        	 //TODO : remove later. done to simplify json building
         	 tmp['Citizenship'] = obj['Citizenship'];
         	 tmp[2007] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
 			 tmp[2008] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
@@ -103,8 +204,6 @@ function ready(error,world, asylumRequests){
         	 tmp[obj['Periods']][obj['Sex']][obj['Age']] =  +obj['number'];
         	 //console.log(JSON.stringify(tmp));
         	 asylum[obj["Country"]]  = tmp;
-        	//var tmp = {'Citizenship' : obj['Citizenship'], obj['Periods'] : { 'Total': obj['numbers'], obj['Sex'] : { obj['Age'] : obj['numbers'] }}};
-        	//asylum[obj["Country"]] = {'Citizenship' : obj['Citizenship'], obj['Periods'] :{ Total:obj['numbers'], obj['Sex']:{ obj['Age']:obj['numbers'] }}};
         }
        else{
          tmp[obj['Periods']]['Total'] += +obj['number'];
