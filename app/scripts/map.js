8940f2b38f7fb6a59718bd1aef32d5858ecfd394
heytitle <pat.chormai@gmail.com>
2016-01-28 03:14:43 +0100

    First attempt of adjustign map

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -2,21 +2,12 @@
 function Map( id ){
     this.element = d3.select(id);
 
-    /*var rect = document.getElementById('map').offsetWidth
-    this.width  = rect.width;
-    this.height = rect.height;*/
-
     this.width  = document.getElementById('map').offsetWidth;
     this.height = this.width/2;
 
     var projection = d3.geo.mercator()
     	.translate([(this.width/2)-30, (this.height/2)+50])
     	.scale( this.width / 2 / Math.PI);
-
-    /*var projection = d3.geo.mercator()
-    	.center([80,40])
-        .translate([(this.width/2), (this.height/2)])
-    	.scale( this.width / 3 / Math.PI) */
 
     var path = d3.geo.path().projection(projection);
     this.path = path;
@@ -45,17 +36,10 @@ Map.prototype.draw = function(){
 
     var path = this.path;
 
-	var graticule = d3.geo.graticule();
-	this.svg.append("path")
-     .datum(graticule)
-     .attr("class", "graticule")
-     .attr("d", path);
-
-
     this.svg.append("path")
-   .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
-   .attr("class", "equator")
-   .attr("d", path);
+       .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
+       .attr("class", "equator")
+       .attr("d", path);
 
     country.enter().insert("path")
         .attr("class", "country")
@@ -63,9 +47,6 @@ Map.prototype.draw = function(){
         .attr("id", function(d,i) { return d.id; })
         .attr("title", function(d,i) { return d.properties.name; });
 
-    //fill color
-    this.colorMap(2008);        
-	
 	country.on("click", this.onClick );
 
 }
@@ -77,8 +58,8 @@ Map.prototype.addToolTip = function(year){
 	 //offsets for tooltips
     var boundary = this.element.node().getBoundingClientRect();
     var offsetL = boundary.left+20;
-    var offsetT = boundary.top+10;   			  
-        			  
+    var offsetT = boundary.top+10;
+
     //tooltips
     country.on("mousemove", function(d,i) {
         var mouse = d3.mouse(self.svg.node()).map( function(d) { return parseInt(d); } );
@@ -86,26 +67,26 @@ Map.prototype.addToolTip = function(year){
         self.tooltip.classed("hidden", false)
              .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
              .html( function() {
-             	if(asylum[d.properties.name]) 
+             	if(asylum[d.properties.name])
              		return d.properties.name+"::"+year+"::"+asylum[d.properties.name][year]['Total']
-             	else 
+             	else
              		return d.properties.name+"::"+year;
              	});
 
       })
       .on("mouseout",  function(d,i) {
         self.tooltip.classed("hidden", true);
-      });	  			 
-   
+      });
+
 }
 
 Map.prototype.colorMap = function(year){
     var self     = this;
     var present = [];
-    
+
     //Define default colorbrewer scheme
 	var colorSchemeSelect = "Oranges";
-	var colorScheme = colorbrewer[colorSchemeSelect]; 
+	var colorScheme = colorbrewer[colorSchemeSelect];
 
 
 	//define default number of quantiles
@@ -146,11 +127,14 @@ Map.prototype.colorMap = function(year){
 		.enter()
 		.append('g').attr('class', 'legend');
 
-	legend
-		.append('rect')
-		.attr("x",this.width - (this.width-30))
+
+    var legendX = 10;
+    var legendY = this.height - 28* quantiles;
+
+	legend.append('rect')
+		.attr("x", legendX )
 		.attr("y", function(d, i) {
-		   return ((self.height-200)+(i * 20));
+		   return (legendY +(i * 20));
 		})
 	   .attr("width", 10)
 	   .attr("height", 10)
@@ -159,25 +143,19 @@ Map.prototype.colorMap = function(year){
 	   .style("fill", function(d){return d;});
 		   //the data objects are the fill colors
 
-	legend
-		.append('text')
-		.attr("x", this.width - (this.width-45)) //leave 5 pixel space after the <rect>
+	legend.append('text')
+		.attr("x", legendX + 15 ) //leave 5 pixel space after the <rect>
 		.attr("y", function(d, i) {
-		   return ((self.height-200)+(i * 20));
+		   return (legendY+(i * 20));
 		})
 		.attr("dy", "0.8em") //place text one line *below* the x,y point
-		.text(function(d,i) {
+		.text( function(d,i) {
 		    var extent = color.invertExtent(d);
 		    //extent will be a two-element array, format it however you want:
-		    var format = d3.format("0.2f");
+		    var format = d3.format(".3s");
 		    return format(+extent[0]) + " - " + format(+extent[1]);
 		});
 
     this.addToolTip(year);
-	//console.log(present);
-	//downloading json
-    /*var url = 'data:text/json;charset=utf8,' + encodeURIComponent(present);
-	window.open(url, '_blank');
-	window.focus();*/
 
 }
